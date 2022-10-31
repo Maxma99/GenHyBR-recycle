c3ff7049f5a4c4dd43d01fc88970bfa9e40e86e1
Max <41200067+Maxma99@users.noreply.github.com>
2022-10-31 22:06:20 +0800

    Update dFMT_Simulation_recon_protocol.m

--- FMT_toolbox/STIFT_v1/dFMT_Simulation_recon_protocol.m
+++ FMT_toolbox/STIFT_v1/dFMT_Simulation_recon_protocol.m
@@ -42,20 +42,19 @@ exp_dFMT.drawReconSTIFT;
 
 
 %% show ground truth (code has error)
-%             
-% gd =  load("ground_truth.mat");
-% iso_int = 0.4;
-% ground_truth = gd.fluo_g_disp;
-% obj_bound = max(exp_dFMT.node);
-% x_display_T = permute(ground_truth,[2,1,3]);
-% [m,n,p] = size(x_display_T);
-% dl = [obj_bound(2)  obj_bound(1) obj_bound(3)]./[m-1 n-1 p-1];
-% [X,Y,Z] = meshgrid(0:(n-1),0:(m-1),0:(p-1));
-% X=X*dl(2);
-% Y=Y*dl(1);
-% Z=Z*dl(3);
-% % x_display_T(x_display_T<(max(x_display_T(:))*0.3))=0; %to reduce the artefact
-% figure(exp_dFMT.figHdl_ExpSetting),hold on
-% patch(isosurface(X,Y,Z,x_display_T,max(x_display_T(:))*iso_int), ...
-%     'facecolor', '#00ffff', 'edgecolor', '#00ff00');
-% hold off
+            
+gd =  load("ground_truth.mat");
+ground_truth = gd.fluo_g_disp;
+obj_bound = max(exp_dFMT.node);
+x_display_T = permute(ground_truth,[2,1,3]);
+[m,n,p] = size(x_display_T);
+dl = [obj_bound(2)  obj_bound(1) obj_bound(3)]./[m-1 n-1 p-1];
+[X,Y,Z] = meshgrid(0:(n-1),0:(m-1),0:(p-1));
+X=X*dl(2);
+Y=Y*dl(1);
+Z=Z*dl(3);
+% x_display_T(x_display_T<(max(x_display_T(:))*0.3))=0; %to reduce the artefact
+figure(exp_dFMT.figHdl_ExpSetting),hold on
+patch(isosurface(X,Y,Z,x_display_T,max(x_display_T(:))*0.01), ...
+    'facecolor', '#00ffff', 'edgecolor', 'none');
+hold off
